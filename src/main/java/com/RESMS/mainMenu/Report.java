ae65e87b7e58c998c5b0ad5d08807308281cdecc
John Lloyd Unida <johnlloydunida0@gmail.com>
2024-07-30 07:57:46 +0800

    Tapos naaaa

--- src/main/java/com/RESMS/mainMenu/Report.java
+++ src/main/java/com/RESMS/mainMenu/Report.java
@@ -7,6 +7,10 @@ package com.RESMS.mainMenu;
 import java.awt.Color;
 import com.RESMS.libs.object.*;
 import com.RESMS.libs.fileSystem.Read;
+import com.RESMS.myapps.AdminMain;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +23,80 @@ public class Report extends javax.swing.JFrame {
     /**
      * Creates new form Report
      */
+    public class CSVReportGenerator{
+        
+    public void generateCSVReport(ArrayList<IData> items, String filePath) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                Class<?> currentClass = null;
+                ArrayList<PaymentMethod> paymentMethods = new ArrayList<>();
+
+                // for non-offer items; get payment method
+                for (IData item : items) {
+                    if (item instanceof Offer) {
+                        Offer offer = (Offer) item;
+                        paymentMethods.add(offer.getPaymentMethod());
+                        if (currentClass == null || !item.getClass().equals(currentClass)) {
+                            writer.newLine();
+                            writeHeader(writer, item);
+                            currentClass = item.getClass();
+                        }
+                        writer.write(offer.toCSV());
+                        writer.newLine();
+                    } else {
+                        if (currentClass == null || !item.getClass().equals(currentClass)) {
+                            writer.newLine();
+                            writeHeader(writer, item);
+                            currentClass = item.getClass();
+                        }
+                        writer.write(item.toCSV());
+                        writer.newLine();
+                    }
+                }
+
+                // for payment method
+                if (!paymentMethods.isEmpty()) {
+                    writer.newLine();
+                    currentClass = null; // reset current class
+                    for (PaymentMethod paymentMethod : paymentMethods) {
+                        if (currentClass == null || !paymentMethod.getClass().equals(currentClass)) {
+                            writer.newLine();
+                            writeHeader(writer, paymentMethod);
+                            currentClass = paymentMethod.getClass();
+                        }
+                        writer.write(paymentMethod.toCSV());
+                        writer.newLine();
+                    }
+                }
+
+                System.out.println("CSV report generated successfully.");
+            } catch (IOException e) {
+                System.out.println("An error occurred while generating the CSV report.");
+                e.printStackTrace();
+            }
+        } 
+
+    private void writeHeader(BufferedWriter writer, Object item) throws IOException {
+            if (item instanceof Employee) {
+                writer.write("Employee ID, Last Name, First Name, Credentials, Password");
+            } else if (item instanceof Buyer) {
+                writer.write("Buyer ID, Last Name, First Name");
+            } else if (item instanceof Property) {
+                writer.write("Property ID, Owner, Lot, Block, SRP, Size, Description, Reservation");
+            } else if (item instanceof Reservation) {
+                writer.write("Reservation ID, Buyer ID, Price, Due Date");
+            } else if (item instanceof Offer) {
+                writer.write("Offer ID, Currency, Property ID");
+            } else if (item instanceof Transaction) {
+                writer.write("Transaction ID, Agent ID, Buyer ID, Offer ID");
+            } else if (item instanceof Cash) {
+                writer.write("Cash Payment ID, Final Price");
+            } else if (item instanceof Installment) {
+                writer.write("Installment Payment ID, Total Amount, Downpayment, Interest Rate, Number of Years");
+            }
+            // Add headers for other classes similarly
+            writer.newLine();
+        }
+    }
     public Report() {
         initComponents();
         this.getContentPane().setBackground(new Color(255, 255, 255));
@@ -40,7 +118,9 @@ public class Report extends javax.swing.JFrame {
     
     private void adminTransactionReport() {
         for (Employee employee : Read.getEmployees()) {
-            this.agentTransactionReport(employee);
+            if (employee.getCredentials().contentEquals("Agent")) {
+                this.agentTransactionReport(employee);
+            }
         }
     }
     
@@ -88,11 +168,13 @@ public class Report extends javax.swing.JFrame {
                 report += "Due Date: " + reservation.getDueDate() + "\n";
                 report += "Offers:\n";
                 for (Offer offerData: offers) {
-                    report += "    " + offerData.getId() + ":" + offerData.getPaymentMethod().getFinalPrice() + " " + offerData.getCurrency() + "(";
-                    if (offerData.getPaymentMethod() instanceof Cash) {
-                        report += "Cash)\n";
-                    }else {
-                        report += "Installment)\n";
+                    if (offerData.getCurrentProperty().getId().contentEquals(propertyData.getId())) {
+                        report += "    " + offerData.getId() + ":" + offerData.getPaymentMethod().getFinalPrice() + " " + offerData.getCurrency() + "(";
+                        if (offerData.getPaymentMethod() instanceof Cash) {
+                            report += "Cash)\n";
+                        }else {
+                            report += "Installment)\n";
+                        }
                     }
                 }
                 report += "\n";
@@ -153,6 +235,11 @@ public class Report extends javax.swing.JFrame {
         saveBtn.setBackground(new java.awt.Color(142, 202, 230));
         saveBtn.setFont(new java.awt.Font("Inter_FXH", 0, 24)); // NOI18N
         saveBtn.setText("Save Report");
+        saveBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                saveBtnMouseClicked(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Inter_FXH", 1, 48)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(2, 48, 71));
@@ -188,6 +275,20 @@ public class Report extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void saveBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_saveBtnMouseClicked
+        ArrayList<IData> allData = new ArrayList<>();
+        allData.addAll(Read.getEmployees());
+        allData.addAll(Read.getBuyers());
+        allData.addAll(Read.getProperties());
+        allData.addAll(Read.getReservations());
+        allData.addAll(Read.getOffers());
+        allData.addAll(Read.getTransactions());
+        
+        CSVReportGenerator reportGenerator = new CSVReportGenerator();
+        
+        reportGenerator.generateCSVReport(allData, "Admin_ReportGenerated.csv");
+    }//GEN-LAST:event_saveBtnMouseClicked
 
     /**
      * @param args the command line arguments
--- src/main/java/com/RESMS/myapps/AdminMain.java
+++ src/main/java/com/RESMS/myapps/AdminMain.java
@@ -16,6 +16,7 @@ import com.RESMS.libs.object.Property;
 import com.RESMS.libs.object.Reservation;
 import com.RESMS.libs.object.Transaction;
 import com.RESMS.mainMenu.MenuFrame;
+import com.RESMS.mainMenu.Report;
 import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
@@ -54,81 +55,6 @@ public class AdminMain extends javax.swing.JFrame {
     
     public AdminMain() {
         initComponents();
-    }
-    
-    public class CSVReportGenerator{
-        
-    public void generateCSVReport(ArrayList<IData> items, String filePath) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-                Class<?> currentClass = null;
-                ArrayList<PaymentMethod> paymentMethods = new ArrayList<>();
-
-                // for non-offer items; get payment method
-                for (IData item : items) {
-                    if (item instanceof Offer) {
-                        Offer offer = (Offer) item;
-                        paymentMethods.add(offer.getPaymentMethod());
-                        if (currentClass == null || !item.getClass().equals(currentClass)) {
-                            writer.newLine();
-                            writeHeader(writer, item);
-                            currentClass = item.getClass();
-                        }
-                        writer.write(offer.toCSV());
-                        writer.newLine();
-                    } else {
-                        if (currentClass == null || !item.getClass().equals(currentClass)) {
-                            writer.newLine();
-                            writeHeader(writer, item);
-                            currentClass = item.getClass();
-                        }
-                        writer.write(item.toCSV());
-                        writer.newLine();
-                    }
-                }
-
-                // for payment method
-                if (!paymentMethods.isEmpty()) {
-                    writer.newLine();
-                    currentClass = null; // reset current class
-                    for (PaymentMethod paymentMethod : paymentMethods) {
-                        if (currentClass == null || !paymentMethod.getClass().equals(currentClass)) {
-                            writer.newLine();
-                            writeHeader(writer, paymentMethod);
-                            currentClass = paymentMethod.getClass();
-                        }
-                        writer.write(paymentMethod.toCSV());
-                        writer.newLine();
-                    }
-                }
-
-                System.out.println("CSV report generated successfully.");
-            } catch (IOException e) {
-                System.out.println("An error occurred while generating the CSV report.");
-                e.printStackTrace();
-            }
-        } 
-
-    private void writeHeader(BufferedWriter writer, Object item) throws IOException {
-            if (item instanceof Employee) {
-                writer.write("Employee ID, Last Name, First Name, Credentials, Password");
-            } else if (item instanceof Buyer) {
-                writer.write("Buyer ID, Last Name, First Name");
-            } else if (item instanceof Property) {
-                writer.write("Property ID, Owner, Lot, Block, SRP, Size, Description, Reservation");
-            } else if (item instanceof Reservation) {
-                writer.write("Reservation ID, Buyer ID, Price, Due Date");
-            } else if (item instanceof Offer) {
-                writer.write("Offer ID, Currency, Property ID");
-            } else if (item instanceof Transaction) {
-                writer.write("Transaction ID, Agent ID, Buyer ID, Offer ID");
-            } else if (item instanceof Cash) {
-                writer.write("Cash Payment ID, Final Price");
-            } else if (item instanceof Installment) {
-                writer.write("Installment Payment ID, Total Amount, Downpayment, Interest Rate, Number of Years");
-            }
-            // Add headers for other classes similarly
-            writer.newLine();
-        }
     }
 
     /**
@@ -308,17 +234,7 @@ public class AdminMain extends javax.swing.JFrame {
 
     private void GenerateReportButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GenerateReportButtonActionPerformed
         // TODO add your handling code here:
-        ArrayList<IData> allData = new ArrayList<>();
-        allData.addAll(Read.getEmployees());
-        allData.addAll(Read.getBuyers());
-        allData.addAll(Read.getProperties());
-        allData.addAll(Read.getReservations());
-        allData.addAll(Read.getOffers());
-        allData.addAll(Read.getTransactions());
-        
-        CSVReportGenerator reportGenerator = new CSVReportGenerator();
-        
-        reportGenerator.generateCSVReport(allData, "Admin_ReportGenerated.csv");
+        new Report().setVisible(true);
         
     }//GEN-LAST:event_GenerateReportButtonActionPerformed
 
